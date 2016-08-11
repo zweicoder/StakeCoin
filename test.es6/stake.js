b800896ca2900502417ef2387d0b79f0338a0e78
zweicoder <zhiwei_chong@mymail.sutd.edu.sg>
2016-08-12 01:23:40 +0800

    Add snapshot functions

---
+++ test.es6/stake.js
@@ -0,0 +1,51 @@
+import utils from './utils.js';
+
+contract('StakeCoin#stake', (accounts) => {
+  const user = accounts[0];
+  const deposit = 1;
+  const depositInWei = web3.toWei(deposit, 'ether');
+  const id = 'stakeCoin';
+
+  it('should setup', () => {
+    const stakeCoin = StakeCoin.deployed();
+    return stakeCoin.deposit({
+      from: user,
+      value: depositInWei
+    }).then(()=> utils.saveSnapshot())
+  })
+
+  it('should fail to stake above sender balance', () => {
+    const stakeCoin = StakeCoin.deployed();
+    return stakeCoin.stake(id, deposit + 1)
+      .then(() => {
+        assert.fail(0, 1)
+      }, (e) => {
+        assert.isOk(e)
+      })
+  })
+
+  it('should stake coins on a string identifier', ()=>{
+    const stakeCoin = StakeCoin.deployed();
+    const amountStaked = deposit;
+    return stakeCoin.stake(id, amountStaked)
+    .then(()=>{
+      const p1 = stakeCoin.getBalanceInEth.call()
+      .then((balance)=> assert.equal(balance, deposit - amountStaked, 'Balance should reduce by the amount staked'))
+
+      const p2 = stakeCoin.getValueOf.call(id)
+        .then((value)=> assert.equal(value, amountStaked, 'Value of id should increase due to amount staked'))
+
+      const p3 = stakeCoin.getStake.call(user, id)
+        .then((amount) => assert.equal(amount, amountStaked), 'stakeOf user on id should increase')
+
+      return Promise.all([p1, p2, p3])
+    })
+  })
+
+  it('should revert to saved snapshot', ()=>{
+    const stakeCoin = StakeCoin.deployed();
+    return utils.revertSnapshot()
+      .then(()=> stakeCoin.getBalanceInEth.call())
+      .then((balance)=> assert.equal(balance, deposit))
+  })
+})
---
+++ test.es6/utils.js
@@ -0,0 +1,40 @@
+function rpc(method, arg) {
+  var req = {
+    jsonrpc: "2.0",
+    method: method,
+    id: new Date().getTime()
+  };
+  if (arg)
+    req.params = arg;
+
+  return new Promise((resolve, reject) => {
+    web3.currentProvider.sendAsync(req, (err, result) => {
+      if (err) return reject(err)
+      if (result && result.error) {
+        return reject(new Error("RPC Error: " + (result.error.message || result.error)))
+      }
+      resolve(result)
+    })
+  })
+}
+
+export default {
+  snap: null,
+  saveSnapshot () {
+    return rpc('evm_snapshot')
+      .then((res)=>{
+        this.snap = res.result;
+      })
+  },
+
+  revertSnapshot () {
+    return rpc('evm_revert', [this.snap])
+  },
+
+  setTime(newTime) {
+  const baseURL = 'http://localhost:8546'
+  return axios.post(baseURL, {
+    time: newTime
+  })
+}
+}
