e0fcde3c58510720cb209d3a3f32c380656dfd51
zweicoder <zhiwei_chong@mymail.sutd.edu.sg>
2016-08-14 00:36:30 +0800

    Move unstake test out

--- test.es6/stake.js
+++ test.es6/stake.js
@@ -149,39 +149,4 @@ contract('StakeCoin#stake', (accounts) => {
       })
   })
 
-  it('should unstake coins', () => {
-    const stakeCoin = StakeCoin.deployed();
-    const [id1, id2] = ['id_1', 'id_2'];
-    return Rpc.revertSnapshot()
-      .then(() => stakeCoin.stake(id1, ethToWei(0.5)))
-      .then(() => stakeCoin.stake(id2, ethToWei(0.5)))
-      .then(() => stakeCoin.stake(id1, ethToWei(0.5), {
-        from: user2
-      }))
-      .then(() => stakeCoin.unstake(id1))
-      .then(() => {
-        const p1 = checkBalanceEquals(stakeCoin, deposit - 0.5, 'Balance should increase by 0.5')
-        const p2 = checkValueEquals(stakeCoin, id1, 0.5, 'Value of id_1 should reduce by 0.5')
-        const p3 = checkStakeEquals(stakeCoin, user1, id1, 0, 'stakeOf user1 on id1 should zero')
-
-        return Promise.all([p1, p2, p3])
-      })
-      .then(() => stakeCoin.unstake(id1))
-      .then(() => {
-        const p1 = checkBalanceEquals(stakeCoin, deposit - 0.5, 'Should be no-op')
-        const p2 = checkValueEquals(stakeCoin, id1, 0.5, 'Should be no-op')
-        const p3 = checkStakeEquals(stakeCoin, user1, id1, 0, 'Should be no-op')
-
-        return Promise.all([p1, p2, p3])
-      })
-      .then(() => stakeCoin.unstake(id2))
-      .then(() => {
-        const p1 = checkBalanceEquals(stakeCoin, deposit, 'Balance should increase by another 0.5')
-        const p2 = checkValueEquals(stakeCoin, id2, 0, 'Value of id_2 should reduce by 0.5')
-        const p3 = checkStakeEquals(stakeCoin, user1, id2, 0, 'stakeOf user1 on id2 should zero')
-
-        return Promise.all([p1, p2, p3])
-      })
-  })
-
 })
